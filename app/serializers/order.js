5d8f748b88533c6e3da6aec33e448f9ba7b59a92
kaka <huxinghai1988@gmail.com>
2017-04-12 22:06:46 +0800

    order custom serializers format

--- app/serializers/order.js
+++ app/serializers/order.js
@@ -1,15 +1,19 @@
 import DS from 'ember-data';
+import Application from "./application";
 
-export default DS.RESTSerializer.extend(DS.EmbeddedRecordsMixin, {
+export default Application.extend(DS.EmbeddedRecordsMixin, {
   primaryKey: "order_no",
   attrs: {
     product: {serialize: false, deserialize: 'records'}
   },
-  extract(store, primaryModelClass, payload, id, requestType){
+  normalizeResponse(store, primaryModelClass, payload, id, requestType){
     let{ code, data, msg } =  payload
     payload[Ember.String.pluralize(primaryModelClass.modelName)] = data.rows
     payload["meta"] = {total_pages: data.total_pages, total_count: data.total_count, page: data.page}
-    delete payload.data  
+    
+    delete payload.data
+    delete payload.code  
+    delete payload.msg
     return this._super(...arguments)
   }
 
